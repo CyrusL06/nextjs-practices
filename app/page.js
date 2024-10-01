8f25eac3adb8f32135ba6584d5c8b9321b4fd343
CyrusLorenzo <120500547+CyrusL06@users.noreply.github.com>
2024-10-01 00:38:48 +0000

    Naigating Between Pages - Right Way

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+
+      {/* THis approach habe a flaw */}
+      {/* Because now we have another page its not single page */}
+      {/* Next Js allows us to  */}
+      <p>
+        {/* <a href="/about">About Us</a>{" "} */}
+        {/* The way we use Anchor in Next */}
+        <Link href="/about">About Us Next</Link>
+      </p>
     </main>
   );
 }
